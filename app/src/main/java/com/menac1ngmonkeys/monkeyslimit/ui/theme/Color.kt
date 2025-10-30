516cc7722612bc9d5de0e668b6819ee9b45baad9
Hazel Handrata <71081063+kittyofheaven@users.noreply.github.com>
2025-10-30 16:25:54 +0700

    fix: change Purple80 color

--- app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
+++ app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.menac1ngmonkeys.monkeyslimit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
+val Purple80 = Color(0xFFD0BCFA)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
