6cd3c428baf71238a65a2cb262a5af6945ddd87e
LidyaLaura <lidyalaura04@gmail.com>
2025-10-31 21:36:21 +0700

    fix: fix color pallete for light mode

--- app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
+++ app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package com.menac1ngmonkeys.monkeyslimit.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFA)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//Light mode
+val Black= Color(0xFF052224)
+val Green1 = Color(0xFF2B5D54)
+val Green2 = Color(0xFF447069)
+val Green3 = Color(0xFF809E98)
+val Green4 = Color(0xFFDFF7E2)
+val Yellow = Color(0xFFFCBD09)
+val Red = Color(0xFFBF0003)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//Dark mode
