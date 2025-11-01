4d57b751c326ff8460bcc25e7e03d9d68d5d763a
LidyaLaura <lidyalaura04@gmail.com>
2025-11-01 22:59:51 +0700

    Update Color.kt

--- app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
+++ app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
@@ -3,12 +3,17 @@ package com.menac1ngmonkeys.monkeyslimit.ui.theme
 import androidx.compose.ui.graphics.Color
 
 //Light mode
-val Black= Color(0xFF052224)
-val Green1 = Color(0xFF2B5D54)
-val Green2 = Color(0xFF447069)
-val Green3 = Color(0xFF809E98)
-val Green4 = Color(0xFFDFF7E2)
-val Yellow = Color(0xFFFCBD09)
-val Red = Color(0xFFBF0003)
+val LBlack= Color(0xFF052224)
+val LGreen1 = Color(0xFF2B5D54)
+val LGreen2 = Color(0xFF447069)
+val LGreen3 = Color(0xFF809E98)
+val LGreen4 = Color(0xFFDFF7E2)
+val LYellow = Color(0xFFFCBD09)
+val LRed = Color(0xFFBF0003)
 
 //Dark mode
+val DBlack = Color(0xFF000000)
+val DGrey = Color(0xFF232121)
+val DOrange = Color(0xFFFCBD09)
+val DYellow = Color(0xFFE2E326)
+val DRed = Color(0xFFFF0707)
