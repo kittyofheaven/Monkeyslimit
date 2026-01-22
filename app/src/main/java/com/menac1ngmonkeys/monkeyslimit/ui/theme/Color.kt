18ac99d50319b701b995a14a3be458da78b09ed2
MatthewA77 <andersonmatthew148@gmail.com>
2026-01-22 16:12:24 +0700

    Update color palette and theme color assignments
    Corrected hex values for custom colors in Color.kt and added a new PinkRed color. Updated Theme.kt to assign new and existing colors to Material theme roles with detailed comments, improving color consistency and clarity across the app's UI.

--- app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
+++ app/src/main/java/com/menac1ngmonkeys/monkeyslimit/ui/theme/Color.kt
@@ -13,11 +13,11 @@ val LightGreen100 = Color(0xFFDFF7E2)
 val LightYellow = Color(0xFFFCBD09)
 val LightRed = Color(0xFFBF0003)
 
-val Wheat = Color(0xFF9E3B6)
-val Sunglow = Color(0xFFBCE6B)
-val GoldenRod = Color(0xFD5A007)
-val Avocado = Color(0xF6C8B08)
-val DrabDarkBrown = Color(0xF2B2202)
+val Wheat = Color(0xFFF9E2B6)
+val Sunglow = Color(0xFFFACF69)
+val GoldenRod = Color(0xFFD5A007)
+val Avocado = Color(0xFF6C8B08)
+val DrabDarkBrown = Color(0xFF2B2202)
 
 //Dark mode
 val DarkBlack = Color(0xFF000000)
@@ -25,3 +25,5 @@ val DarkGrey = Color(0xFF232121)
 val DarkOrange = Color(0xFFFCBD09)
 val DarkYellow = Color(0xFFE2E326)
 val DarkRed = Color(0xFFFF0707)
+
+val PinkRed = Color(0xFFF35450)
