4c9ea7b9ea59fc4b3b7a30dbeb869b8c57906b86
MatthewA77 <andersonmatthew148@gmail.com>
2026-02-19 14:57:47 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,8 +48,8 @@ android {
         applicationId = "com.menac1ngmonkeys.monkeyslimit"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.0"
+        versionCode = 5
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
